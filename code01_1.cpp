2ab0b624592386a7d85e8a0f7ad25004a09a72ff
arsh7644 <arshsujan4@gmail.com>
2026-03-25 14:46:47 +0530

    Add files via upload

---
+++ code01_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter value of a: ";
+    cin >> a;
+
+    cout << "Enter value of b: ";
+    cin >> b;
+
+    c = a + b;
+    cout << "sum = " << c;
+
+    return 0;
+}
+
---
+++ part1_.6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+
+    cout << "Enter the value of A: ";
+    cin >> a;
+    cout << "Enter the Value of B: ";
+    cin >> b;
+
+    c = a * b;
+
+
+    cout << "THE area of the rectangle: " << c << endl;
+
+    return 0;
+}
+
